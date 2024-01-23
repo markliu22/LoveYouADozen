9ccf5e89210f4c1ff8bff983020e6a1fcb0a8196
Mark Liu <markliu202@gmail.com>
2024-01-23 00:43:35 -0500

    can download image

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,3 +1,24 @@
+// import React from 'react';
+
+// const Summary = ({ answers, bouquetImagePath }) => {
+//   console.log('Answers:', answers);
+
+//   return (
+//     <div>
+//       <h2>Summary</h2>
+//       {/* Render other summary information */}
+//       {bouquetImagePath && (
+//         <div>
+//           <img src={bouquetImagePath} alt="Final Bouquet" />
+//           <p>Other summary details...</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Summary;
+
 import React from 'react';
 
 const Summary = ({ answers, bouquetImagePath }) => {
@@ -11,6 +32,10 @@ const Summary = ({ answers, bouquetImagePath }) => {
         <div>
           <img src={bouquetImagePath} alt="Final Bouquet" />
           <p>Other summary details...</p>
+          {/* Download link */}
+          <a href={bouquetImagePath} download="BouquetImage.png" style={{ display: 'block', marginTop: '10px' }}>
+            Download Bouquet Image
+          </a>
         </div>
       )}
     </div>
